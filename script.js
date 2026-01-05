a0a8f0de1f608b26b27afda9965fb664e538f276
_xyz <72780929+xyren122@users.noreply.github.com>
2026-01-05 23:57:50 +0800

    eee

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+
+function displayToScreen(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = ""
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value)
+    }
+    catch(error) {
+        display.value = "Syntax Error"
+    }
+}
